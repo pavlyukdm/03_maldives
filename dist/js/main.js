ed22a767e68e12dd5ab2e47f2bce4ac203ccad05
PDM-pavlyukdm <pavlyukdm@gmail.com>
2020-04-30 11:20:12 +0300

    2

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,8 @@
-(function () {
-	console.log('main.js is included - OK!');
-})();
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function() {
+	// Toggle class "is-active"
+	hamburger.classList.toggle("is-active");
+	// Do something else, like open/close menu
+});
