5c61e6365815386bdccc04cb5254ae702e1b7373
PDM-pavlyukdm <pavlyukdm@gmail.com>
2020-05-05 21:02:47 +0300

    05.05.20

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,65 @@ hamburger.addEventListener("click", function() {
 	hamburger.classList.toggle("is-active");
 	// Do something else, like open/close menu
 });
+
+var headerSwiper = new Swiper('.slider-container', {
+	slidesPerView: 1,
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'progressbar',
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
+/**Formatting figures to 0x format with leading zero */
+function zeroPad(num, places) {
+	var zero = places - num.toString().length + 1;
+	return Array(+(zero > 0 && zero)).join("0") + num;
+}
+
+/**Block with slides pagination indicators */
+$(function () {
+	var slidesNumber = headerSwiper.slides.length;
+	var currentIndex = headerSwiper.activeIndex + 1;
+
+	$('.main-slider .progressbar-slide-index').html(zeroPad(currentIndex, 2));
+	$('.main-slider .progressbar-slide-total').html(zeroPad(slidesNumber, 2));
+	$('.main-slider .slider-counter .current-slide').html(zeroPad(currentIndex, 2));
+	$('.main-slider .slider-counter .total-slides').html(zeroPad(slidesNumber, 2));
+	headerSwiper.on('slideChange', function () {
+	$('.main-slider .progressbar-slide-index').html(zeroPad(headerSwiper.activeIndex + 1, 2));
+	$('.main-slider .slider-counter .current-slide').html(zeroPad(headerSwiper.activeIndex + 1, 2));
+	})
+
+	$('.main-slider .swiper-button .prev-slide-counter').html(zeroPad(headerSwiper.activeIndex, 2));
+	$('.main-slider .swiper-button .next-slide-counter').html(zeroPad(headerSwiper.activeIndex + 2, 2));
+
+	headerSwiper.on('slideChange', function () {
+		$('.main-slider .swiper-button .prev-slide-counter').html(zeroPad(headerSwiper.activeIndex, 2));
+		$('.main-slider .swiper-button .next-slide-counter').html(zeroPad(headerSwiper.activeIndex + 2, 2));
+	})
+})
+
+function slidesBg () {
+	var prevItem = headerSwiper.activeIndex - 1;
+	var nextItem = headerSwiper.activeIndex + 1;
+
+	bgPrev = headerSwiper.imagesToLoad[prevItem] === undefined ? '' : headerSwiper.imagesToLoad[prevItem].src;
+	bgNext = headerSwiper.imagesToLoad[nextItem] === undefined ? '' : headerSwiper.imagesToLoad[nextItem].src;
+
+	if (bgPrev) {
+		$('.main-slider .swiper-button-prev').css('background', 'url('+bgPrev+')');
+	}
+	if (bgNext) {
+		$('.main-slider .swiper-button-next').css('background', 'url('+bgNext+')');
+	}
+}
+
+/**Bg images on arrows navigations */
+$(function () {
+	slidesBg ()
+	headerSwiper.on('slideChange', slidesBg)
+})
